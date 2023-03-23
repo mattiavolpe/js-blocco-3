b796fe0244916f664b78ae1e83aa3c28fa960a08
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-23 12:16:31 +0100

    INIT with capitalized words

---
+++ js-snack-2/assets/js/main.js
@@ -0,0 +1,12 @@
+/*
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
+*/
+
+const words = ["fabio", "lucA", "aLEx", "MAttiA", "fabrIZio", "LorENzO", "ROBERTO", "franCESco"];
+
+const capitalizedWords = words.map((word) => {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+});
+console.log(capitalizedWords);
