d8bc1c8e481dfed28e9252af4d02049385574beb
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-23 12:55:59 +0100

    INIT With classified animals

---
+++ js-snack-3/assets/js/main.js
@@ -0,0 +1,37 @@
+/*
+Snack 3
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+Crea un nuovo array con la lista dei mammiferi.
+*/
+
+const animals = [
+
+  {
+    nome: 'leone',
+    famiglia: 'felidi',
+    classe: 'mammiferi'
+  },
+
+  {
+    nome: 'cane',
+    famiglia: 'canidi',
+    classe: 'mammiferi'
+  },
+
+  {
+    nome: 'gallina',
+    famiglia: 'fasianidi',
+    classe: 'uccelli'
+  }
+
+];
+
+const mammalAnimals = animals.filter(animal => animal.classe.toLowerCase() == "mammiferi");
+console.log(mammalAnimals);
